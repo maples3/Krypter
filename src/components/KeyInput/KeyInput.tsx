a28bbc80f3e88c2c7593619ddfea676c94d620e2
Anthony <anthonymapes3@gmail.com>
2019-11-06 18:23:25 -0500

    Added keyword input section

---
+++ src/components/KeyInput/KeyInput.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class KeyInput extends React.Component {
+    render() {
+        return <div>
+            <label>Keyword</label>
+            <input></input>
+            <label>Key letter</label>
+            <input></input>
+        </div>
+    }
+}
+
+export default KeyInput;
--- src/components/KeySection/KeySection.tsx
+++ src/components/KeySection/KeySection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import KeyLetter from '../KeyLetter/KeyLetter';
 import './KeySection.css'
+import KeyInput from '../KeyInput/KeyInput';
 
 class KeySection extends React.Component {
     render() {
@@ -13,6 +14,7 @@ class KeySection extends React.Component {
         console.log(letters);
 
         return <div className="KeySection">
+            <KeyInput></KeyInput>
             <div className="rowHeader"><p>Ciphertext</p><p>Plaintext</p></div>
             {letters.map((value, index) => {
             return <KeyLetter ctLetter={value}></KeyLetter>
